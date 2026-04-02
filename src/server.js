d4cb5f66e08bd8fefc508e9629da3b846724a925
Nazar Rudenko <nazar.rudenko@sick.de>
2026-04-02 13:14:30 +0200

    feat(server): remove sockets from global and rooms array on close, render rooms online

--- src/server.js
+++ src/server.js
@@ -23,17 +23,39 @@ for (let i = 1; i < defaultRoomsNumber + 1; i++) {
 
 console.log(state.roomsObj)
 
+
 const server = net.createServer((socket) => {
     const uuid = uuidv4();
     socket.id = uuid
     console.log("client joined")
 
     state.userGlobalArray.push(socket)
+    console.log(state.userGlobalArray.length)
+    
+
+    // render rooms for user
+    function renderRooms() {
+        let roomString = ``
+        for (let i = 1; i < Object.keys(state.roomsObj).length + 1; i++) {
+            const roomLength = state.roomsObj[`room${i}`].roomUsersArray.length
+            if (!roomLength == 0){
+                roomString += `room #${i}: ${roomLength} online\n`
+            } else{
+                roomString += `room #${i}: empty\n`
+            }
+        }
+        console.log(roomString)
+
+        state.userGlobalArray.forEach((client) => {
+            client.write(roomString)
+        })
+    }
+
+    renderRooms()
 
     socket.on("data", (data) => {
-        if (!checkUserInput(data, state, socket)) return
-
         if (!socket.room) {
+            if (!checkUserInput(data, state, socket)) return
             const userData = data.toString().trim()
             const userRoom = state.roomsObj[`room${userData}`]
             socket.room = userRoom
@@ -47,16 +69,29 @@ const server = net.createServer((socket) => {
                 client.write(msg)
             });
         }
-
+        renderRooms()
     })
-
 
     socket.on("error", (error) => {
         console.error("Server Error: ", error.message)
+    })
+
+    socket.on("close", () => {
+        //clear global array
+        state.userGlobalArray = state.userGlobalArray.filter((client) => {
+            return client !== socket
+        })
+
+        // clear room array
+        socket.room.roomUsersArray = socket.room.roomUsersArray.filter((client) => {
+            return client !== socket
+        })
+
+        console.log("client disconnected: ", state.userGlobalArray.length)
+        renderRooms()
     })
 })
 
 server.listen(PORT, () => {
     console.log("server listening")
 })
-
