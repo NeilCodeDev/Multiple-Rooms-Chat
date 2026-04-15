94345352b37ed01efeab783326b2519a064707da
Nazar Rudenko <nazar.rudenko@sick.de>
2026-04-15 09:15:46 +0200

    refactor(renderRooms): improve readability, clean up the render function

--- src/utils/renderRooms.js
+++ src/utils/renderRooms.js
@@ -3,15 +3,17 @@ export default function renderRooms(state) {
     let roomString = ``
     for (let i = 1; i < Object.keys(state.roomsObj).length + 1; i++) {
         const roomLength = state.roomsObj[`room${i}`].roomUsersArray.length
-        if (!roomLength == 0){
-            roomString += `room #${i}: ${roomLength} online\n`
-        } else{
-            roomString += `room #${i}: empty\n`
+        const roomMaxUsers = state.roomsObj[`room${i}`].maxUsers
+        const roomName = state.roomsObj[`room${i}`].roomName ? `(${state.roomsObj[`room${i}`].roomName})` : ""
+
+        if (roomLength > 0) {
+            roomString += `room #${i}${roomName}: ${roomLength}/${roomMaxUsers} online\n`
+        } else {
+            roomString += `room #${i}${roomName}: ${roomLength}/${roomMaxUsers}\n`
         }
+
+
     }
     console.log(roomString)
-    // state.userGlobalArray.forEach((client) => {
-    //     client.write(roomString)
-    // })
     return roomString
 }
