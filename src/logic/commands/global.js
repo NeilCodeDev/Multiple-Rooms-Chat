75c467130e0437650d8e482326c48ed79a34828d
Nazar Rudenko <nazar.rudenko@sick.de>
2026-04-17 11:36:56 +0200

    feat(global): in /help command listing add /join command

--- src/logic/commands/global.js
+++ src/logic/commands/global.js
@@ -1,6 +1,6 @@
 // help
 function displayHelp(socket) {
-    socket.write(`\nCommands Console: \n /help - all commands listed\n /leave - leave a room\n /create | room name | max users number (e.g. 5) - create a room\n /username (input) - set custom username\n /delete room | (room name)\n /kick (username) - kick user in the room`)
+    socket.write(`\nCommands Console: \n /help - all commands listed\n /join (number of the room, e.g. 1)\n /leave - leave a room\n /create | room name | max users number (e.g. 5) - create a room\n /username (input) - set custom username\n /delete room | (room name)\n /kick (username) - kick user in the room`)
 }
 
 // username
